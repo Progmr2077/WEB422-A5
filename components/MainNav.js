3748a35aac82853e3725b2099780774ce61a46e7
jrivera17 <jrivera17@myseneca.ca>
2025-03-26 17:07:48 -0400

    new changes

--- components/MainNav.js
+++ components/MainNav.js
@@ -6,27 +6,31 @@ import { useState } from 'react';
 export default function MainNav() {
     const router = useRouter();
     const [search, setSearch] = useState('');
+    const [isExpanded, setIsExpanded] = useState(false); // State to control navbar expansion
 
     const handleSearch = (e) => {
         e.preventDefault();
         router.push(`/artwork?title=true&q=${search}`);
+        setIsExpanded(false); // Close navbar after search
     };
 
     return (
-        <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
+        <Navbar bg="primary" variant="dark" expand="lg" fixed="top" expanded={isExpanded}>
             <Container>
                 <Navbar.Brand>Jacob Rivera</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsExpanded(!isExpanded)} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Link href="/" passHref legacyBehavior>
-                            <Nav.Link>Home</Nav.Link>
+                            <Nav.Link onClick={() => setIsExpanded(false)}>Home</Nav.Link>
                         </Link>
                         <Link href="/search" passHref legacyBehavior>
-                            <Nav.Link>Advanced Search</Nav.Link>
+                            <Nav.Link onClick={() => setIsExpanded(false)}>Advanced Search</Nav.Link>
                         </Link>
                     </Nav>
+                    &nbsp;
                     <Form className="d-flex" onSubmit={handleSearch}>
+                    &nbsp;
                         <Form.Control
                             type="search"
                             placeholder="Search"
