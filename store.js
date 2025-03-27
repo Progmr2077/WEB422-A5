5db50b6bcbdab1e28e512eb6107b2f765f877ecb
jrivera17 <jrivera17@myseneca.ca>
2025-03-26 20:21:44 -0400

    add searchHistoryAtom to store user search queries

--- store.js
+++ store.js
@@ -2,3 +2,7 @@ import { atom } from 'jotai';
 
 // Create a Jotai atom to store the list of favourite artworks
 export const favouritesAtom = atom([]);
+
+// Create a Jotai atom to collect the search query
+
+export const searchHistoryAtom = atom({});
