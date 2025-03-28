c33b030ce4a5da0dfcac43b7cb4af5f5e94eb059
jrivera17 <jrivera17@myseneca.ca>
2025-03-27 21:47:08 -0400

    refactor store.js to update searchHistoryAtom to store search history as an array and improve comments

--- store.js
+++ store.js
@@ -1,8 +1,7 @@
 import { atom } from 'jotai';
 
-// Create a Jotai atom to store the list of favourite artworks
+// Jotai atom to store the list of favourite artworks
 export const favouritesAtom = atom([]);
 
-// Create a Jotai atom to collect the search query
-
-export const searchHistoryAtom = atom({});
+// Jotai atom to store the search history as an array
+export const searchHistoryAtom = atom([]);
